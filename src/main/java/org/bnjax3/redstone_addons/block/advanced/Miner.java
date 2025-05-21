3a97c4f1f3745eb32b4737381cac7cd51a232e66
bnjax3 <153530323+bnjax3@users.noreply.github.com>
2025-05-21 15:57:45 -0300

    pepe

--- src/main/java/org/bnjax3/redstone_addons/block/advanced/Miner.java
+++ src/main/java/org/bnjax3/redstone_addons/block/advanced/Miner.java
@@ -11,6 +11,8 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.state.BooleanProperty;
 import net.minecraft.state.properties.BlockStateProperties;
+import net.minecraft.util.Mirror;
+import net.minecraft.util.Rotation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraft.world.server.ServerWorld;
@@ -28,9 +30,19 @@ public class Miner extends DirectionalBlock {
     public void tick(BlockState blockState, ServerWorld serverWorld, BlockPos pos, Random random) {
         if (blockState.getValue(POWERED))
         {
-
+            damageBlockFacing();
         }
         
         super.tick(blockState, serverWorld, pos, random);
+    }
+    public BlockState rotate(BlockState p_185499_1_, Rotation p_185499_2_) {
+        return p_185499_1_.setValue(FACING, p_185499_2_.rotate(p_185499_1_.getValue(FACING)));
+    }
+
+    public BlockState mirror(BlockState p_185471_1_, Mirror p_185471_2_) {
+        return p_185471_1_.rotate(p_185471_2_.getRotation(p_185471_1_.getValue(FACING)));
+    }
+    public void damageBlockFacing(){
+
     }
 }
