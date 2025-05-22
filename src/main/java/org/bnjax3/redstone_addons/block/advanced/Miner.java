85068886ebd37041320ff2b79c57025d0778c9af
bnjax3 <153530323+bnjax3@users.noreply.github.com>
2025-05-22 12:14:02 -0300

    no hice una mierda

--- src/main/java/org/bnjax3/redstone_addons/block/advanced/Miner.java
+++ src/main/java/org/bnjax3/redstone_addons/block/advanced/Miner.java
@@ -1,7 +1,9 @@
 package org.bnjax3.redstone_addons.block.advanced;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.DirectionalBlock;
+import net.minecraft.item.BlockItemUseContext;
 import net.minecraft.state.BooleanProperty;
+import net.minecraft.state.DirectionProperty;
 import net.minecraft.state.properties.BlockStateProperties;
 import net.minecraft.util.Mirror;
 import net.minecraft.util.Rotation;
@@ -16,17 +18,18 @@ public class Miner extends DirectionalBlock {
     public Miner(Properties properties) {
         super(properties);
     }
+    public static final DirectionProperty FACING = DirectionalBlock.FACING;
     public static final BooleanProperty POWERED = BlockStateProperties.POWERED;
-    @SuppressWarnings("deprecated")
     @Override
     public void tick(BlockState blockState, ServerWorld serverWorld, BlockPos pos, Random random) {
+        serverWorld.getBlockState().
         if (blockState.getValue(POWERED))
         {
             damageBlockFacing();
         }
-        
         super.tick(blockState, serverWorld, pos, random);
     }
+
     public BlockState rotate(BlockState p_185499_1_, Rotation p_185499_2_) {
         return p_185499_1_.setValue(FACING, p_185499_2_.rotate(p_185499_1_.getValue(FACING)));
     }
@@ -34,6 +37,11 @@ public class Miner extends DirectionalBlock {
     public BlockState mirror(BlockState p_185471_1_, Mirror p_185471_2_) {
         return p_185471_1_.rotate(p_185471_2_.getRotation(p_185471_1_.getValue(FACING)));
     }
+
+    public BlockState getStateForPlacement(BlockItemUseContext p_196258_1_) {
+        return this.defaultBlockState().setValue(FACING, p_196258_1_.getNearestLookingDirection().getOpposite());
+    }
+
     public void damageBlockFacing(){
 
     }
